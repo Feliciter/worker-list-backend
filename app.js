afcc107c1fb0afb03c6e8f90c3055cea60405ef8
Feliciter <padakluev@gmx.com>
2020-09-01 02:50:54 +0300

    fix: set

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use('/employees', require('./employees/employee.controller'));
 app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const port =  process.env.MONGODB_URI === 'production' ? (process.env.PORT || 80) : 4000;
 const server = app.listen(port, function () {
     console.log('Server listening on port ' + port);
 });
